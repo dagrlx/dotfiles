dfa57b26388402c096c97da779769e8b423634f6
dagrlx <dagrlx@gmail.com>
2024-07-20 10:45:44 -0400

    Update .config/nix-darwin/home/core.nix

--- dot_config/nix-darwin/home/core.nix
+++ dot_config/nix-darwin/home/core.nix
@@ -316,16 +316,19 @@
         set -g @catppuccin_window_middle_separator "  █"
 
         set -g @catppuccin_window_default_fill "number"
+        set -g @catppuccin_window_default_text "#W"
 
         set -g @catppuccin_window_current_fill "number"
-        set -g @catppuccin_window_current_text "#{pane_current_path}"
+        set -g @catppuccin_window_current_text "#W"
 
-        set -g @catppuccin_status_modules_right "application session host date_time"
+        set -g @catppuccin_status_modules_right "directory application session host date_time"
         set -g @catppuccin_status_left_separator  ""
         set -g @catppuccin_status_right_separator " "
         set -g @catppuccin_status_right_separator_inverse "yes"
         set -g @catppuccin_status_fill "all"
         set -g @catppuccin_status_connect_separator "no"
+
+        set -g @catppuccin_directory_text "#{pane_current_path}"
 
         # Configuracion para mostrar correctamente image preview en yazi
         set -g allow-passthrough on
