e4fcdffa1f6a7c99a6762193b428631f6f01c235
dagrlx <dagrlx@gmail.com>
2024-08-09 15:30:17 -0400

    Update .config/nvim/init.lua Add .config/nvim/lua/plugins/formatting.lua Add .config/nvim/lua/plugins/lsp/lspconfig.lua Add .config/nvim/lua/plugins/lsp/mason.lua Add .config/nvim/lua/plugins/undotree.lua Add .config/nvim/tmp/mason.lua

---
+++ dot_config/nvim/lua/plugins/lsp/mason.lua
@@ -0,0 +1,52 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
+
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
+
+		local mason_tool_installer = require("mason-tool-installer")
+
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"tsserver", -- JavaScript
+				"html", -- HTML
+				"cssls", -- CSS
+				"lua_ls", -- Lua
+				"pyright", -- Python
+				"jsonls", -- JSON
+				"yamlls", -- YAML
+				"marksman", -- Markdown
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint", -- python linter
+				"eslint_d", -- JavaScript linter
+			},
+		})
+	end,
+}
