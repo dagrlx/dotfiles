286727176dc7b1d5332ec28237bcf2c9fdb9d6c5
dagrlx <dagrlx@gmail.com>
2024-08-10 12:52:50 -0400

    Update .config/nvim/lua/plugins/bufferline.lua

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -3,7 +3,10 @@
 return {
 	"akinsho/bufferline.nvim",
 	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
+	dependencies = {
+		"nvim-tree",
+		"nvim-web-devicons",
+	},
 	event = "VeryLazy",
 	enabled = true,
 	opts = {
