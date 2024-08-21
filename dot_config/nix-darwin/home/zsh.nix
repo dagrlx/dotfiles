39a1dd89ea0314be885bd515cdc6794fc8d9e5dd
dagrlx <dagrlx@gmail.com>
2024-08-20 20:46:43 -0400

    Update .chezmoi.toml Update .config/nix-darwin/home/zsh.nix

--- dot_config/nix-darwin/home/zsh.nix
+++ dot_config/nix-darwin/home/zsh.nix
@@ -1,10 +1,13 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    autosuggestion.enable = true;  # Habilita las sugerencias de autocompletado
-    syntaxHighlighting.enable = true;  # Habilita el resaltado de sintaxis
+    autosuggestion.enable = true; # Habilita las sugerencias de autocompletado
+    autosuggestion.highlight = "fg=#ff00ff,bg=cyan,bold,underline";
+    autosuggestion.strategy = [ "history" ];
+    syntaxHighlighting.enable = true; # Habilita el resaltado de sintaxis
     syntaxHighlighting.highlighters = [ "brackets" ];
+    historySubstringSearch.enable = true; # Enable history substring search.
 
     #initExtra = builtins.readFile ./zshrc;
 
@@ -27,35 +30,42 @@
 
       export HOSTNAME=$(hostname)
 
-      '';
+    '';
 
     shellAliases = {
-      rustscan = "docker run -it --rm --name rustscan --platform linux/amd64 rustscan/rustscan";
+      rustscan =
+        "docker run -it --rm --name rustscan --platform linux/amd64 rustscan/rustscan";
       "..." = "cd ../..";
       update = "darwin-rebuild switch --flake ~/.config/nix-darwin/";
       uflake = "nix flake update --flake ~/.config/nix-darwin";
       ngc = "nix-collect-garbage -d";
       sgc = "sudo nix-collect-garbage -d";
       bcp0 = "brew cleanup --prune=0";
-      flushdns = "sudo dscacheutil -flushcache && sudo killall -HUP mDNSResponder";
-      allow_app = "codesign --sign - --force --deep @$ && xattr -d com.apple.quarantine @$"; #Para de-quarantine un app de MacOS
+      flushdns =
+        "sudo dscacheutil -flushcache && sudo killall -HUP mDNSResponder";
+      allow_app =
+        "codesign --sign - --force --deep @$ && xattr -d com.apple.quarantine @$"; # Para de-quarantine un app de MacOS
       n = "nano -clS";
-      fzn="fzf --preview 'bat --style=numbers --color=always {}' | xargs -n1 nvim";
-      sshp="ssh -o ProxyJump=sabaext";
+      fzn =
+        "fzf --preview 'bat --style=numbers --color=always {}' | xargs -n1 nvim";
+      sshp = "ssh -o ProxyJump=sabaext";
 
-      urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-      urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+      urldecode =
+        "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
+      urlencode =
+        "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
 
     };
 
     history = {
       size = 10000;
-      ignoreDups = true; #Ignora duplicados
+      ignoreDups = true; # Ignora duplicados
       #ignoreAllDups = true; # NO almacenar duplicados ** Investigar porque indica que opcion no existe para Macos
-      ignoreSpace = true; #Elimina del historial los comandos que empiecen con un espacio
+      ignoreSpace =
+        true; # Elimina del historial los comandos que empiecen con un espacio
       extended = true; # Guarda el timestamp
-      share = true; #Comparte el historial de comando entre sesiones
-      expireDuplicatesFirst = true; #elimina los duplicados primero.
+      share = true; # Comparte el historial de comando entre sesiones
+      expireDuplicatesFirst = true; # elimina los duplicados primero.
     };
 
     # oh-my-zsh = {
@@ -66,7 +76,7 @@
     #   #theme = "robbyrussell";
     # };
 
-#    initExtra = "eval \"\$(zoxide init zsh)\"";
+    #    initExtra = "eval \"\$(zoxide init zsh)\"";
 
   };
 }
