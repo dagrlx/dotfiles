a1dc98e244651cb0bb76efb28cb7b29a5936b0cd
dagrlx <dagrlx@gmail.com>
2024-12-23 15:26:42 -0300

    Update .dotfiles/nix-darwin/modules/apps.nix

--- dot_dotfiles/nix-darwin/modules/apps.nix
+++ dot_dotfiles/nix-darwin/modules/apps.nix
@@ -123,6 +123,7 @@
       "chezmoi"
       #"yadm"
       "age" # Is a simple, modern and secure file encryption tool, format, and Go library.
+      "atuin" # Shell history with SQLite
 
       #"talosctl"
       "ntfs-3g-mac"
