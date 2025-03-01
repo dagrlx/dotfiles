34f5c0b66770cea30cd46ea1b74b580c5f590b98
dagrlx <dagrlx@gmail.com>
2025-03-01 11:31:15 -0300

    change option enableSudoTouchIdAuth = true is depreciated, add reattach

--- dot_dotfiles/nix-darwin/modules/system.nix
+++ dot_dotfiles/nix-darwin/modules/system.nix
@@ -150,7 +150,6 @@
   };
 
   # Add ability to used TouchID for sudo authentication
-  # security.pam.enableSudoTouchIdAuth = true; # depreciated
   security.pam.services.sudo_local.touchIdAuth = true;
   
   # This fixes Touch ID for sudo not working inside tmux and screen.
