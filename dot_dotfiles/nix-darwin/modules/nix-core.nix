2764ba565ca29c87e946c091786cfba8c4ad8ac0
dagrlx <dagrlx@gmail.com>
2025-03-11 15:39:49 -0300

    re-config of garbage collection and optimise store

--- dot_dotfiles/nix-darwin/modules/nix-core.nix
+++ dot_dotfiles/nix-darwin/modules/nix-core.nix
@@ -18,6 +18,9 @@
   # do garbage collection weekly to keep disk usage low
   nix.gc = {
     automatic = lib.mkDefault true;
+    #dates = "weekly";
+    # If using nix-darwin, use this to run on 0th day of every week:
+    interval = { Weekday = 0; Hour = 0; Minute = 0; };
     options = lib.mkDefault "--delete-older-than 7d";
   };
 
@@ -25,12 +28,24 @@
   # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
   # Disable auto-optimise-store because of this issue:
   #   https://github.com/NixOS/nix/issues/7273
-  # "error: cannot link '/nix/store/.tmp-link-xxxxx-xxxxx' to '/nix/store/.links/xxxx': File exists"
+  # "error: cannot link '/nix/store/.tmp-link-xxxxx-xxxxx' to '/nix/store/.links/xxxx': File exists
+  # (see https://github.com/NixOS/nix/issues/7273#issuecomment-2295429401)
+  # Tip This option only applies to new files, so we recommend manually optimising your nix store when first setting this option.
+  nix.optimise = {
+        automatic = true;
+        interval = [ 
+            {
+                Hour = 8;
+                Minute = 30;
+                Weekday = 7;
+            }
+        ];
+    };
 
-  # in trial period (see https://github.com/NixOS/nix/issues/7273#issuecomment-2295429401)
-  nix.optimise.automatic = true;
-
+  # The store can be optimised during every build.
+  # If true, this may slow down builds, as discussedc here https://github.com/NixOS/nix/issues/6033
   nix.settings = { auto-optimise-store = false; };
+
   # Turn off NIX_PATH warnings now that we're using flakes
   #system.checks.verifyNixPath = false;
 }
